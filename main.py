0484513888a92e2f5eb4a7390b0311a8f16b61b0
MartinHajek69 <159123515+MartinHajek69@users.noreply.github.com>
2024-09-25 06:40:04 +0000

    bla bla

---
+++ main.py
@@ -0,0 +1,18 @@
+jmeno = "Martin"
+Prijmeni = "Hajek"
+
+print(jmeno)
+print(Prijmeni)
+
+vek = int(input("Zadejte věk: "))
+print("Váš věk", vek)
+
+print(len(jmeno))
+print(len(Prijmeni))
+
+hodnota = 6
+
+for i in range(5):
+    hodnota += 3
+    print(hodnota)
+print("vysledna hodnota je 21")
