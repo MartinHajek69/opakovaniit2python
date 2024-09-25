ac709243062bae8b90b2cb189bf0b8bdcde23b51
MartinHajek69 <159123515+MartinHajek69@users.noreply.github.com>
2024-09-25 08:08:04 +0000

    bla bla

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import random
 jmeno = "Martin"
 Prijmeni = "Hajek"
 
@@ -6,9 +7,11 @@ print(Prijmeni)
 
 vek = int(input("Zadejte věk: "))
 print("Váš věk", vek)
-
+print("Počet písmen ve jméně:")
 print(len(jmeno))
+print("Počet písmen ve Příjmení:")
 print(len(Prijmeni))
+print("-------------------------")
 
 hodnota = 6
 
@@ -16,3 +19,4 @@ for i in range(5):
     hodnota += 3
     print(hodnota)
 print("vysledna hodnota je 21")
+
